41f0e71a24f35eb9da4c5f5a6ccb8f089b6d0e79
侯文君 <junerver@qq.com>
2026-06-24 08:30:03 +0800

    chore: enable kover plugin for parser modules

--- palette-mermaid/build.gradle.kts
+++ palette-mermaid/build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kover)
 }
 
 kotlin {
